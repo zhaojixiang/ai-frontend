cafb57a2010f9943ee7a531776f28129a3639f0f
冯胥 <fengxu@tinman.cn>
2025-10-29 10:17:10 +0800

    feat: 删除接口携带userid refs #432090

--- src/lib/request/index.ts
+++ src/lib/request/index.ts
@@ -19,7 +19,7 @@ const userId = localStorage.getItem('userId');
 // 模拟调试header信息，跳过授权检测
 export const DEBUG_HEADER_INFO = Os.debug
   ? {
-      'X-UAGW-userId': userId || '200051446',
+      'X-UAGW-userId': userId,
       'X-UAGW-authMode': 1
     }
   : {};
