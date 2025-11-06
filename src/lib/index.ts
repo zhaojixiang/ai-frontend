2ee4a3f5e9270998ccb7a7d16a55589fef75caf2
赵吉祥 <zhaojixiang@tinman.cn>
2025-11-06 14:01:52 +0800

    merge master refs #379832

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,5 @@ export { default as bridge } from './bridge';
 export { default as navigate } from './navigate';
 export { default as Os } from './os';
 export { default as request } from './request';
-export { default as showPage } from './showPage';
 export { default as toast } from './toast';
 export { default as Utils } from './utils/index';
